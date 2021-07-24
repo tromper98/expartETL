9a5ac1eb8f9a9065844e156bdaa09486a45c58f7
tromper98 <56966408+tromper98@users.noreply.github.com>
2021-07-24 09:38:40 +0300

    Added load_API_config function

--- APIConnection.py
+++ APIConnection.py
@@ -5,4 +5,16 @@ import yaml
 
 import pandas as pd
 import requests as rq
+from yaml.loader import Loader
+
+
+def load_API_config():
+    """
+    Загрузка параметров подключения к API сайта (URL, token) 
+    и списка валют 
+    """
+    with open("config.yaml") as cf:
+        read_data = yaml.safe_load(cf)
+    return read_data["API_CONNECTION"]
+
 
