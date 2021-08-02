c6658c510a5009ccccb19752fddffcc57026c1f6
tromper98 <56966408+tromper98@users.noreply.github.com>
2021-08-02 21:46:17 +0300

    Added annotation into ParseConfig

--- scripts/ParseConfig.py
+++ scripts/ParseConfig.py
@@ -1,10 +1,9 @@
 #Набор функций для обращения к yaml файлу конфигурации и извлечения из него данных
 import traceback
-from typing import List
+from typing import Dict, List, Any, Union
 import yaml
 from pathlib import Path
 from dataclasses import dataclass
-
 
 from Logger import create_logger
 
@@ -25,15 +24,15 @@ class DB_config:
     port: str
     databases: str
 
-def read_config(section):
+def read_config(section : str) -> Union[Dict, None]:
     """
     Чтение конфигурационного файла. возвращает указанную секцию файла  
     """
     try:
-        myself = Path(__file__).resolve()
-        path = myself.parents[1] / 'config.yaml'
+        myself: Path = Path(__file__).resolve()
+        path: str = myself.parents[1] / 'config.yaml'
         with open(path) as cf:
-            read_data = yaml.safe_load(cf)
+            read_data: Dict = yaml.safe_load(cf)
         Logger.info("Чтение файла 'config.yaml' выполнено успешно")       
         return read_data[section]
     except FileNotFoundError:
@@ -41,12 +40,12 @@ def read_config(section):
         return None
 
 #Читает config.yaml и возвращает секцию API_CONNECTION 
-def load_API_config():
+def load_API_config() -> Union[Dict, None]:
     """
     Загрузка параметров подключения к API сайта (URL, token) 
     и списка валют 
     """
-    api_config = read_config("API_CONNECTION")
+    api_config: Union[Dict, None]= read_config("API_CONNECTION")
     if (api_config):
         return api_config
     else:
@@ -54,36 +53,36 @@ def load_API_config():
 
 
 #Парсит полученную секцию из load_API_config и возвращает параметры поэлементно
-def parse_API_config():
+def parse_API_config()-> Union[API_config, None]:
     """
     Получить данные для подключения к API из конфигурационного файла
     """
-    config = load_API_config()
+    config: Union[Dict, None] = load_API_config()
     if config:
-        API_params = API_config(config["URL"], config["Token"], config["Base"], ",".join(config["Currency"]))
+        API_params: API_config = API_config(config["URL"], config["Token"], config["Base"], ",".join(config["Currency"]))
         return API_params
     else:
         Logger.error("Не удалось получить данные для подключения к API",  traceback.format_exc())
 
 #Получает данные для подключения к БД из config.yaml
-def load_database_config():
+def load_database_config() -> Union[Dict, None]:
     """
     Получить параметры подключения к БД из файла конфигурации
     """
-    database_config = read_config("DATABASE")
+    database_config: Union[Dict, None] = read_config("DATABASE")
     if database_config:
         return database_config
     else:
         return None
 
 #парсер конфигурации подключения к базе данных
-def parse_database_config():
+def parse_database_config() -> Union[DB_config, None]:
     """
     Извлечение данных о подключения к базе данных
     """
-    config = load_database_config()
+    config: Union[Dict, None] = load_database_config()
     if config:
-        DB_params = DB_config(config["Host"], config["UserName"], config["Password"],
+        DB_params: DB_config = DB_config(config["Host"], config["UserName"], config["Password"],
                               config["Port"], config["Databases"])
         return  DB_params
     else:
